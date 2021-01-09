f3606b9082681d6626f612acd85b8c7dc4d16d29
Muchenski <muchenski.dev@gmail.com>
2021-01-09 10:49:30 -0300

    Create Position Class

--- Seção 12 - Sistema de Xadrez/Seção 13 - Sistema de Xadrez/Program.cs
+++ Seção 12 - Sistema de Xadrez/Seção 13 - Sistema de Xadrez/Program.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using System;
+using tabuleiro;
+
 namespace Seção_12___Sistema_de_Xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Position p = new Position(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
---
+++ Seção 12 - Sistema de Xadrez/Seção 13 - Sistema de Xadrez/tabuleiro/Position.cs
@@ -0,0 +1,25 @@
+﻿
+namespace tabuleiro
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Collumn { get; set; }
+
+        public Position()
+        {
+
+        }
+
+        public Position(int row, int collumn)
+        {
+            Row = row;
+            Collumn = collumn;
+        }
+
+        public override string ToString()
+        {
+            return $"{Row},{Collumn}";
+        }
+    }
+}
